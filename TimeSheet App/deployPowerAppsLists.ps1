7e8a1dfa14639f07220f443f73ab326a57caef94
April Dunnam <april.dunnam@thrivefast.com>
2020-01-17 20:35:56 -0600

    Timesheet Power App
    Template for a timesheet application in Power Apps.  Uses SharePoint lists as a data source to hold the information.  Includes a PowerShell script to deploy the lists.

---
+++ TimeSheet App/deployPowerAppsLists.ps1
@@ -0,0 +1,22 @@
+﻿#To deploy the list templates you need to first install the PNP PowerShell Commandlets#
+$pnpInstalled = Get-Module SharePointPnPPowerShell* -ListAvailable | Select-Object Name,Version | Sort-Object Version -Descending
+if($pnpInstalled -ne $null){
+$siteURL = Read-Host -Prompt 'Enter the SharePoint Site URL that you want to deploy the lists to'
+
+$path = Read-Host -Prompt 'Enter the complete path (file name included) of the list template you want to deploy'
+
+Connect-PnPOnline -Url $siteURL -UseWebLogin
+
+Apply-PnPProvisioningTemplate -path $path
+}
+else {
+Install-Module SharePointPnPPowerShellOnline
+$siteURL = Read-Host -Prompt 'Enter the SharePoint Site URL that you want to deploy the lists to'
+
+$path = Read-Host -Prompt 'Enter the complete path (file name included) of the list template you want to deploy'
+
+Connect-PnPOnline -Url $siteURL -UseWebLogin
+
+Apply-PnPProvisioningTemplate -path $path
+
+}
